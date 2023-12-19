333ac24a9f9cd5e356d0b811dc67f0842b637f04
kunchamthiru12 <thirumalakuncham123@gmail.com>
2023-12-19 07:49:16 -0800

    hello shell script

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,4 +3,4 @@
 echo "Ramesh: Hello Suresh,Good Morning"
 echo "Suresh: Hi Ramesh,very Good Morning"
 echo "Ramesh: How are you suresh?"
-echo "Suresh:I am good Ramesh, How are you"
+echo "Suresh: I am good Ramesh, How are you"
