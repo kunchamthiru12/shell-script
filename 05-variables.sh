81a264c8af2ca51d017bc838b8d5994da5f95422
kunchamthiru12 <thirumalakuncham123@gmail.com>
2023-12-20 03:15:59 -0800

    added 2nd script

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,4 +4,4 @@ echo "please enter your name"
 
 read UERNAME #the value entered above will be autiomatically attached to username variable
 
-echo "Username is : $USERNAME"
+echo "Username is: $USERNAME"
