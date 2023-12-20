4b9bf5da61c473ab0679fda9a2a4e11db61f0dad
kunchamthiru12 <thirumalakuncham123@gmail.com>
2023-12-20 07:14:48 -0800

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -5,8 +5,9 @@ ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%s)
 R="\e[31m"
 G="\e[32m"
-n="\e[0m"
+N="\e[0m"
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
  echo "script started executing at $TIMESTAMP" &>> $LoGFILE
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -20,16 +21,12 @@ VALIDATE(){
 
 if [ $ID -ne 0 ]
 then
-    echo -e "$R Error:: please run this script with root access $N"
-    exit 1
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
 else
-    echo "you are root user"
-fi
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-yum install mysql -y &>> $LOGFIE
+yum install mysql -y &>> $LOGFILE
 
-VALIDATE $? "Installing MYSQL"
-
-yum install git -y &>> $LOGFILE
-
-VALIDATE $? "Installing GIT"
+VALIDATE $? "Installing MySQL"
