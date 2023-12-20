4a7e237348e2fe5192b0582f21fc6df1db4f6a98
kunchamthiru12 <thirumalakuncham123@gmail.com>
2023-12-20 05:31:08 -0800

    added 2nd script

--- 10-functions.sh
+++ 10-functions.sh
@@ -3,12 +3,12 @@
 ID=$(id -u)
 
 VALIDATE(){
-    if [$? -ne 0]
+    if [$1? -ne 0]
     then
-        echo "ERROR:: installing  is failed"
+        echo "ERROR:: $2 ...   failed"
         exit1
     else
-        echo "Installing   is success"
+        echo "$2 .... success"
     fi  
 }
 
@@ -22,8 +22,8 @@ fi
 
 yum install mysql -y
 
-VALIDATE
+VALIDATE $? "Installing MYSQL"
 
 yum install git -y
 
-VALIDATE
+VALIDATE $? "Installing GIT"
