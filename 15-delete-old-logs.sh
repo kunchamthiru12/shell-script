61ee900a9d63a50fbf6f0e76c108be3b60f68282
kunchamthiru12 <thirumalakuncham123@gmail.com>
2023-12-26 16:22:04 +1200

    roboshopshell-script

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-SOURCE_DIR="/tmp/shellscripts-logs"
+SOURCE_DIR="/tmp/shellscript-logs"
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ 1 -d $SOURCE_DIR ] # ! denotes opposite
+if [ ! -d $SOURCE_DIR ] # ! denotes opposite
 then
-    echo -e "$R source directory : $SOURCE_DIR does not exists. $N".
-
+    echo -e "$R Source directory: $SOURCE_DIR does not exists. $N"
 fi
 
-FILES_TO_DELETE =$(find $SOURCE_DIR  -type f -mtime +14 -name "*.log")
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
-while IFS =read -r line
+while IFS= read -r line
 do
     echo "Deleting file: $line"
     rm -rf $line
-done <<< $Files_To_DELETE    
+done <<< $FILES_TO_DELETE   
