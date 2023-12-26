abfc7bf12ee00ad6f7669dd19de24671e9b550c9
kunchamthiru12 <thirumalakuncham123@gmail.com>
2023-12-26 21:13:54 +1200

    roboshopshell-script

---
+++ mail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TO_TEAM=$1
+ALERT_TYPE=$2
+BODY=$3
+ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
+TO_ADDRESS=$4
+SUBJECT=$5
+
+FINAL_BODY=$(sed -e 's/To_TEAM/$TO_TEAM/g' -e 's/ALERT_TYPE/$ALERT_TYPE/g' -e 's/BODY/$BODY/g' template.html)
+
+echo "$FINAL_BODY" |mail _s "$(echo -e "$SUBJECT\nContent-Type: text/html")""$TO_ADDRESS"
